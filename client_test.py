5dcc90be105cbdd3d9e19d4e597f292ef5310ce0
Deepak Singh <dsingh20800@gmail.com>
2020-05-05 00:35:33 +0100

    Create Patch File

--- client_test.py
+++ client_test.py
@@ -24,10 +24,10 @@ class ClientTest(unittest.TestCase):
 
   """ ------------ Add more unit tests ------------ """
 
-  """ ------------ Assertion to ensure the right ratio is calculated ------------ """
-  def test_getRatio_calculateRatio(self):
+  """ ------------ Assertion to ensure the right ratio is calculated if price_a is greater than price_b ------------ """
+  def test_getRatio_calculateRatioAGreaterThanB(self):
     quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 161.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 150.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
 
@@ -40,15 +40,48 @@ class ClientTest(unittest.TestCase):
     ratio = (price_a / price_b)
 
     self.assertEqual(getRatio(price_a, price_b), ratio) 
+
+  """ ------------ Assertion to ensure the right ratio is calculated if price_b is greater than price_a ------------ """
+  def test_getRatio_calculateRatioBGreaterThanA(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 167.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 133.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+
+    stock, bid_price, ask_price, price  = getDataPoint(quotes[0])
+    price_a = price
+
+    stock, bid_price, ask_price, price  = getDataPoint(quotes[1])
+    price_b = price
+
+    ratio = (price_a / price_b)
+
+    self.assertEqual(getRatio(price_a, price_b), ratio) 
   
-  """ ------------ Assertion to ensure the right ratio is calculated if one of the prices is 0 ------------ """
-  def test_getRatio_calculateRatioPriceIsZero(self):
+  """ ------------ Assertion to ensure the right ratio is calculated if price_a is 0 ------------ """
+  def test_getRatio_calculateRatioPriceAIsZero(self):
     quotes = [
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
 
-    price_a = getDataPoint(quotes[0])
+    price_a = 0
+
+    stock, bid_price, ask_price, price  = getDataPoint(quotes[1])
+    price_b = price
+
+    self.assertEqual(getRatio(price_a, price_b), 0) 
+
+  """ ------------ Assertion to ensure the right ratio is calculated if price_b is 0 ------------ """
+  def test_getRatio_calculateRatioPriceBIsZero(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+
+    stock, bid_price, ask_price, price  = getDataPoint(quotes[0])
+    price_a = price
+    
     price_b = 0
 
     self.assertEqual(getRatio(price_a, price_b), None) 
